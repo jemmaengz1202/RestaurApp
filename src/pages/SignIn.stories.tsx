dd71396b0c5d9172f8202afebbe0e39f48bf22e7
Juan Enríquez <jemmaengz1202@gmail.com>
2019-09-26 00:05:18 -0500

    Add light theme for SingIn.stories

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import WithTheme from "../components/WithTheme";
 import SignIn from "./SignIn";
+import { lightTheme } from "../themes";
 
 storiesOf('SignInForm', module)
   .add('Dark theme', () => (
     <WithTheme>
+      <SignIn />
+    </WithTheme>
+  ))
+  .add('Light theme', () => (
+    <WithTheme theme={lightTheme}>
       <SignIn />
     </WithTheme>
   ));
