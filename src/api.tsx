5345d7997d14abe6dab2209698e19402f725b562
Juan Enríquez <jemmaengz1202@gmail.com>
2019-10-16 07:23:54 -0500

    Replace use-http with @use-hooks/axios

--- src/api.tsx
+++ src/api.tsx
@@ -1,1 +1,8 @@
-export const API_URL = 'http://localhost:3000/api/';
+import axios from 'axios';
+
+export const API_URL = 'http://localhost:3000/api';
+
+export const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
