16a4edf49273f6c55952664612e633f6476aa2f8
Juan Enríquez <jemmaengz1202@gmail.com>
2019-10-21 09:12:21 -0500

    Created insert and edit Producto form

---
+++ src/components/ProductoForm.tsx
@@ -0,0 +1,277 @@
+import { Button, createStyles, Grid, InputAdornment, MenuItem, Paper, Theme } from '@material-ui/core';
+import ImageIcon from '@material-ui/icons/Image';
+import { makeStyles } from '@material-ui/styles';
+import useAxios from '@use-hooks/axios';
+import { Field, Form, Formik, FormikActions } from 'formik';
+import { TextField } from 'formik-material-ui';
+import React, { useState, useEffect } from 'react';
+import { API_URL, axiosInstance } from '../api';
+import { productoSchema } from '../schemas/Producto.schema';
+import Categoria from '../types/categoria';
+import Producto from '../types/producto';
+import CustomizedSnackbar from './CustomizedSnackbar';
+
+type ProductoFormProps = {
+  producto: Partial<Producto>
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    buttonUpload: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1)
+    },
+    buttonSubmit: {
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(2)
+    },
+    image: {
+      maxWidth: '100%',
+      marginBottom: theme.spacing(2)
+    },
+    mainGrid: {
+      maxWidth: '100%',
+    }
+  })
+);
+
+export function ProductoForm(props: ProductoFormProps)  {
+  const classes = useStyles();
+
+  const [producto, setProducto] = useState(props.producto);
+
+  const [isValidImageEdit, setIsValidImageEdit] = useState(false);
+
+  const [imagePreviewUrl, setImagePreviewUrl] = useState<string | ArrayBuffer | null>(producto.imagenUrl ? producto.imagenUrl : null);
+  const [imageFormData, setImageFormData] = useState<FormData | null>(null);
+
+  const { response } = useAxios({
+    axios: axiosInstance,
+    url: '/categorias',
+    method: 'GET',
+    trigger: [],
+  });
+  const listaCategorias: Array<Categoria> = response ? response.data : [];
+
+  const [openSnack, setOpenSnack] = useState(false);
+
+  const handleImageChange = async (e: any, formIsValid: boolean) => {
+    e.preventDefault();
+    const files = e.target.files;
+    if (files.length > 0) {
+      const image = files[0];
+      const formData = new FormData();
+      formData.append('file', image);
+
+      // Para la previsualización de la imagen
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreviewUrl(reader.result);
+      };
+      reader.readAsDataURL(image);
+
+      setImageFormData(formData);
+
+      if (formIsValid) {
+        setIsValidImageEdit(true);
+      }
+    }
+  };
+
+  const handlePrecioFocus = (e: any) => {
+    e.target.select();
+  };
+
+  const handleSnackClose = (event?: any, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSnack(false);
+  };
+
+  const onSubmit = async (values: Partial<Producto>, { setSubmitting, resetForm }: FormikActions<Partial<Producto>>) => {
+    let imagenUrl;
+    if (imageFormData) {
+      try {
+        const res = await axiosInstance({
+          url: '/attachments/images/upload',
+          method: 'POST',
+          data: imageFormData,
+          headers: { 'Content-Type': 'multipart/form-data' }
+        });
+        const imageName = res.data.result.files.file[0].name;
+        imagenUrl = `${API_URL}/attachments/images/download/${imageName}`;
+      } catch (err) {
+        console.log('Error al subir la imagen: ', err);
+      }
+    }
+    const productoBody = { ...values, imagenUrl };
+    const res = await axiosInstance({
+      url: (producto.id ? `/productos/${producto.id}`: '/productos'),
+      method: (producto.id ? 'PATCH': 'POST'),
+      data: productoBody,
+    });
+    setSubmitting(false);
+    if (res) {
+      console.log('Todo OK: ', res);
+      setOpenSnack(true);
+      setImagePreviewUrl(null);
+    }
+    if (producto.imagenUrl) {
+      setImagePreviewUrl(producto.imagenUrl);
+      setProducto(values);
+    }
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={producto}
+      validationSchema={productoSchema}
+      onSubmit={onSubmit}
+      render={({ submitForm, isSubmitting, isValid, values }) => (
+        <Grid
+          className={classes.mainGrid}
+          container
+          spacing={3}
+          justify="center"
+          component="div"
+        >
+          <Grid item xs={8} component={Paper}>
+            <Form>
+              <Field
+                name="nombre"
+                type="text"
+                label="Nombre"
+                component={TextField}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+              />
+              <br />
+              <Field
+                name="precio"
+                type="number"
+                label="Precio"
+                component={TextField}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                onFocus={handlePrecioFocus}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">$</InputAdornment>
+                  )
+                }}
+              />
+              <br />
+              <Field
+                name="descripcion"
+                type="text"
+                label="Descripción"
+                component={TextField}
+                variant="outlined"
+                multiline
+                margin="normal"
+                fullWidth
+                rowsMax="5"
+              />
+              <br />
+              <Field
+                name="categoriaId"
+                type="text"
+                label="Categoría"
+                margin="normal"
+                fullWidth
+                component={TextField}
+                select
+                InputLabelProps={{
+                  shrink: true
+                }}
+                helperText={!producto.id ? 'Campo requerido' : null}
+              >
+                {listaCategorias.map(categoria => (
+                  <MenuItem key={categoria.id} value={categoria.id}>
+                    {categoria.nombre}
+                  </MenuItem>
+                ))}
+              </Field>
+              <br />
+              <input
+                accept="image/*"
+                style={{ display: 'none' }}
+                id="raised-button-file"
+                type="file"
+                onChange={(e) => handleImageChange(e, values == producto)}
+              />
+              <label htmlFor="raised-button-file">
+                <Button
+                  variant="contained"
+                  id="raised-button-file"
+                  color="secondary"
+                  startIcon={<ImageIcon />}
+                  component="span"
+                  className={classes.buttonUpload}
+                >
+                  {imagePreviewUrl || producto.imagenUrl
+                    ? 'Cambiar imagen'
+                    : 'Subir imagen'}
+                </Button>
+              </label>
+              <br />
+              {imagePreviewUrl && (
+                <img
+                  className={classes.image}
+                  src={imagePreviewUrl as any}
+                  alt="Imagen del producto"
+                />
+              )}
+              {!imagePreviewUrl && (
+                <p>No hay imagen guardada.</p>
+              )}
+              <br />
+              <Button
+                variant="contained"
+                color="primary"
+                disabled={isSubmitting || (!isValid && !isValidImageEdit)}
+                onClick={submitForm}
+                fullWidth
+              >
+                {producto.id ? 'Guardar cambios' : 'Guardar'}
+              </Button>
+            </Form>
+            <CustomizedSnackbar
+              open={openSnack}
+              message="Operación exitosa."
+              handleClose={handleSnackClose}
+            />
+          </Grid>
+        </Grid>
+      )}
+    />
+  );
+};
+
+ProductoForm.defaultProps = {
+  producto: {},
+} as Partial<ProductoFormProps>;
+
+type EditProductoFormProps = {
+  id: number
+}
+
+export function EditProductoForm({ id }: EditProductoFormProps) {
+  const {response, loading} = useAxios({
+    axios: axiosInstance,
+    url: `/productos/${id}`,
+    method: 'GET',
+    trigger: [],
+  });
+
+  const producto = response ? response.data : null; 
+
+  if (loading || producto == null) return <>Cargando</>;
+
+  return <ProductoForm producto={ producto } />;
+
+};
