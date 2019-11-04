0b526cb4c481c1b8aa5d04e49146f9d375f7ac81
Juan Enríquez <jemmaengz1202@gmail.com>
2019-11-03 20:55:54 -0600

    Change GeneralContext to always implement methods

--- src/contexts/GeneralContext.ts
+++ src/contexts/GeneralContext.ts
@@ -1,3 +1,4 @@
+import { signInAction, signOutAction, changeTitleAction, changeThemeAction } from './../reducers/general';
 import Usuario from "../types/usuario";
 import { createContext } from "react";
 
@@ -6,16 +7,20 @@ export interface GeneralState {
   user?: Usuario,
   title: string,
   theme?: string,
-  signIn?: (user: Usuario) => void,
-  signOut?: () => void,
-  changeTitle?: (title: string) => void,
-  changeTheme?: (theme: string) => void,
+  signIn: (user: Usuario) => void,
+  signOut: () => void,
+  changeTitle: (title: string) => void,
+  changeTheme: (theme: string) => void,
 };
 
 export const InitialGeneralState = {
   isSignedIn: false,
   title: 'RestaurApp',
   theme: 'dark',
+  signIn: signInAction,
+  signOut: signOutAction,
+  changeTitle: changeTitleAction,
+  changeTheme: changeThemeAction,
 };
 
 export const GeneralContext = createContext<GeneralState>(InitialGeneralState);
