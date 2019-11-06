e481644bbdbc149ed545e93fde6893a0fcabcb08
Juan Enríquez <jemmaengz1202@gmail.com>
2019-11-05 22:51:55 -0600

    Add snackbar data to the state

--- src/contexts/GeneralContext.ts
+++ src/contexts/GeneralContext.ts
@@ -1,6 +1,8 @@
-import { signInAction, signOutAction, changeTitleAction, changeThemeAction, changeSearchAction } from './../reducers/general';
+import { signInAction, signOutAction, changeTitleAction, changeThemeAction, changeSearchAction, openSnackbarAction, closeSnackbarAction } from './../reducers/general';
 import Usuario from "../types/usuario";
 import { createContext } from "react";
+
+export type snackbarVariant = "success" | "warning" | "error" | "info";
 
 export interface GeneralState {
   isSignedIn: boolean,
@@ -8,6 +10,11 @@ export interface GeneralState {
   title: string,
   theme?: string,
   search?: string,
+  snackbarMessage?: string,
+  snackbarOpen?: boolean,
+  snackbarVariant?: snackbarVariant,
+  openSnackbar: (message: string, variant?: snackbarVariant) => void,
+  closeSnackbar: () => void,
   signIn: (user: Usuario) => void,
   signOut: () => void,
   changeTitle: (title: string) => void,
@@ -19,11 +26,14 @@ export const InitialGeneralState = {
   isSignedIn: false,
   title: 'RestaurApp',
   theme: 'dark',
+  snackbarOpen: false,
   signIn: signInAction,
   signOut: signOutAction,
   changeTitle: changeTitleAction,
   changeTheme: changeThemeAction,
   changeSearch: changeSearchAction,
+  openSnackbar: openSnackbarAction,
+  closeSnackbar: closeSnackbarAction,
 };
 
 export const GeneralContext = createContext<GeneralState>(InitialGeneralState);
