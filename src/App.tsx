ebada1f52ae13fd558ea404bb2ec51c755ae43b5
Juan Enríquez <jemmaengz1202@gmail.com>
2019-11-05 22:55:51 -0600

    Change App to fit new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppRouter } from './AppRouter';
+import WithTheme from './components/WithTheme';
+import { darkTheme } from './themes';
+import { GeneralContextContainer } from './components/GeneralContextContainer';
 
-const App: React.FC = () => {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GeneralContextContainer>
+      <WithTheme theme={darkTheme}>
+        <AppRouter />
+      </WithTheme>
+    </GeneralContextContainer>
   );
-}
+};
 
 export default App;
