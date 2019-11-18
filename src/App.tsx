651d16b55620bfcde7cfaa53e37f021b49a51679
Juan Enríquez <jemmaengz1202@gmail.com>
2019-11-17 21:59:01 -0600

    Wrap AppRouter with ConfirmationServiceProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { AppRouter } from './AppRouter';
 import WithTheme from './components/WithTheme';
 import { darkTheme } from './themes';
 import { GeneralContextContainer } from './components/GeneralContextContainer';
+import { ConfirmationServiceProvider } from './components/ConfirmationService';
 
 
 const App = () => {
   return (
     <GeneralContextContainer>
       <WithTheme theme={darkTheme}>
-        <AppRouter />
+        <ConfirmationServiceProvider>
+          <AppRouter />
+        </ConfirmationServiceProvider>
       </WithTheme>
     </GeneralContextContainer>
   );
