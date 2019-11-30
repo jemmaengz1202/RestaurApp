4ff8648610dd168d60b601633ac137298be03378
Juan Enríquez <jemmaengz1202@gmail.com>
2019-11-29 21:23:36 -0600

    Add Producto page component

---
+++ src/pages/Producto.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ProductoViewDialog from '../components/ProductoViewDialog';
+import { useParams, useHistory } from 'react-router';
+
+export default function Producto() {
+  const [open, setOpen] = useState(true);
+  const history = useHistory();
+  const { idprod } = useParams();
+
+  return (
+    <ProductoViewDialog 
+      handleClose={() => {
+        setOpen(false);
+        history.replace('/productos');
+      }}
+      idProducto={Number(idprod)}
+      open={open}
+    />
+  )
+}
