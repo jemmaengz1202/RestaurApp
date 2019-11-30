56cb519bb9f7f97eceff69d0370197584035ea2d
Juan Enríquez <jemmaengz1202@gmail.com>
2019-11-30 16:32:43 -0600

    Add ReactImageMagnify on component

--- src/components/ProductoViewDialog.tsx
+++ src/components/ProductoViewDialog.tsx
@@ -10,6 +10,9 @@ import Slide from "@material-ui/core/Slide";
 import { TransitionProps } from "@material-ui/core/transitions";
 import useAxios from '@use-hooks/axios';
 import { axiosInstance } from '../api';
+import { Grid, Divider, Card } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import ReactImageMagnify from 'react-image-magnify';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,7 +21,13 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     title: {
       marginLeft: theme.spacing(2),
-      flex: 1
+    },
+    link: {
+      textDecoration: 'none', 
+      color: 'white',
+    },
+    textMargin: {
+      marginTop: theme.spacing(1),
     }
   })
 );
@@ -39,7 +48,7 @@ export default function ProductoViewDialog({ idProducto, open, handleClose }: Pr
   const classes = useStyles();
   const { response, loading } = useAxios({
     axios: axiosInstance,
-    url: `/productos/${idProducto}`,
+    url: `/productos/${idProducto}?filter[include][categoria]`,
     method: "GET",
     trigger: []
   });
@@ -66,13 +75,89 @@ export default function ProductoViewDialog({ idProducto, open, handleClose }: Pr
               <CloseIcon />
             </IconButton>
             <Typography variant="h6" className={classes.title}>
-              {producto.nombre}
+              <Link
+                to="/productos"
+                className={classes.link}
+                onClick={handleClose}
+              >
+                Todos los productos
+              </Link>
+            </Typography>
+            <Typography variant="h6" className={classes.title}>
+              /
+            </Typography>
+            <Typography variant="h6" className={classes.title}>
+              <Link
+                to={`/productos/categoria/${producto.categoriaId}`}
+                className={classes.link}
+                onClick={handleClose}
+              >
+                {producto.categoria ? producto.categoria.nombre : ""}
+              </Link>
             </Typography>
           </Toolbar>
         </AppBar>
-        <div>
-
-        </div>
+        <Grid
+          container
+          justify="center"
+          style={{ padding: 32, minHeight: "90vh" }}
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6}>
+            <ReactImageMagnify
+              smallImage={{
+                alt: "Imagen del producto",
+                isFluidWidth: true,
+                src: producto.imagenUrl
+              }}
+              largeImage={{
+                alt: "Imagen del producto",
+                src: producto.imagenUrl,
+                width: 1000,
+                height: 800
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Card style={{ padding: 32 }}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Typography variant="h3">{producto.nombre}</Typography>
+                  <Divider />
+                </Grid>
+                <Grid item xs={12}>
+                  <Grid container alignItems="center">
+                    <Grid item className={classes.textMargin}>
+                      {producto.categoria && (
+                        <Link
+                          style={{
+                            marginRight: 16,
+                            textDecoration: "none",
+                            color: "white"
+                          }}
+                          to={`/productos/categoria/${producto.categoriaId}`}
+                        >
+                          <i>{producto.categoria.nombre}</i>
+                        </Link>
+                      )}
+                    </Grid>
+                    <Grid item className={classes.textMargin}>
+                      <Typography variant="button">
+                        Id: {producto.id}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Typography variant="body2" className={classes.textMargin}>
+                    {producto.descripcion}
+                  </Typography>
+                  <Typography variant="h4" className={classes.textMargin}>
+                    <b>Precio:</b> ${producto.precio}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+        </Grid>
       </Dialog>
     </div>
   );
