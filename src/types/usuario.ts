4d535fb3a36d999ab86ba03960bcf87be1739c93
Juan Enríquez <jemmaengz1202@gmail.com>
2019-12-01 12:33:44 -0600

    Modify usuario type to include rolId and imagenUrl

--- src/types/usuario.ts
+++ src/types/usuario.ts
@@ -2,6 +2,8 @@ export default interface Usuario {
   id: number,
   nombre: string,
   username: string,
-  password?: string,
-  role?: string,
+  password: string,
+  imagenUrl: string,
+  rolId: number,
+  role: string,
 }
