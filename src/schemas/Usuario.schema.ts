ffda0ab26062eb91d6e0a6850206b17cf401bbd4
Juan Enríquez <jemmaengz1202@gmail.com>
2019-12-01 15:25:44 -0600

    Validate unique username at usuario schema

--- src/schemas/Usuario.schema.ts
+++ src/schemas/Usuario.schema.ts
@@ -1,8 +1,32 @@
+import { axiosInstance } from "./../api";
 import * as yup from "yup";
+import adb from "awesome-debounce-promise";
 
 export const usuarioSchema = yup.object().shape({
   nombre: yup.string().required("El elemento es requerido"),
-  username: yup.string().required("El elemento es requerido"),
+  username: yup
+    .string()
+    .required("El elemento es requerido")
+    .test(
+      "unique_username",
+      "Ya hay alguien usando este nombre de usuario",
+      adb(async value => {
+        try {
+          const res = await axiosInstance({
+            url: `/usuarios?filter[where][username]=${encodeURIComponent(
+              value
+            )}`,
+            method: "GET"
+          });
+          if (res.data.length === 0) {
+            return true;
+          }
+          return false;
+        } catch (err) {
+          return true;
+        }
+      }, 500)
+    ),
   imagenUrl: yup
     .string()
     .nullable()
