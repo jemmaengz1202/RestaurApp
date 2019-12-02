3d01170e918c567f44190a0588f04f238a824cfb
Juan Enríquez <jemmaengz1202@gmail.com>
2019-12-01 18:03:45 -0600

    Changes in user schema

--- src/schemas/Usuario.schema.ts
+++ src/schemas/Usuario.schema.ts
@@ -2,7 +2,22 @@ import { axiosInstance } from "./../api";
 import * as yup from "yup";
 import adb from "awesome-debounce-promise";
 
-export const usuarioSchema = yup.object().shape({
+const isUniqueUsername = adb(async value => {
+  try {
+    const res = await axiosInstance({
+      url: `/usuarios?filter[where][username]=${encodeURIComponent(value)}`,
+      method: "GET"
+    });
+    if (res.data.length === 0) {
+      return true;
+    }
+    return false;
+  } catch (err) {
+    return true;
+  }
+}, 500);
+
+const shape = {
   nombre: yup.string().required("El elemento es requerido"),
   username: yup
     .string()
@@ -10,22 +25,7 @@ export const usuarioSchema = yup.object().shape({
     .test(
       "unique_username",
       "Ya hay alguien usando este nombre de usuario",
-      adb(async value => {
-        try {
-          const res = await axiosInstance({
-            url: `/usuarios?filter[where][username]=${encodeURIComponent(
-              value
-            )}`,
-            method: "GET"
-          });
-          if (res.data.length === 0) {
-            return true;
-          }
-          return false;
-        } catch (err) {
-          return true;
-        }
-      }, 500)
+      isUniqueUsername
     ),
   imagenUrl: yup
     .string()
@@ -39,4 +39,25 @@ export const usuarioSchema = yup.object().shape({
     .string()
     .nullable()
     .notRequired()
-});
+};
+
+export const usuarioSchema = yup.object().shape(shape);
+
+const editShape = {
+  nombre: yup.string().required("El elemento es requerido"),
+  username: yup.string().required("El elemento es requerido"),
+  imagenUrl: yup
+    .string()
+    .nullable()
+    .notRequired(),
+  rolId: yup
+    .number()
+    .required("El elemento es requerido")
+    .nullable(),
+  password: yup
+    .string()
+    .nullable()
+    .notRequired()
+};
+
+export const editUsuarioSchema = yup.object().shape(editShape);
