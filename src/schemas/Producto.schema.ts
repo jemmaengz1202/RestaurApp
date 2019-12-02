55d2b193ea93fc6e9b2e5a624b0714bf3ad8a70c
Juan Enríquez <jemmaengz1202@gmail.com>
2019-12-01 18:03:58 -0600

    Changes in producto schema

--- src/schemas/Producto.schema.ts
+++ src/schemas/Producto.schema.ts
@@ -2,29 +2,29 @@ import adb from "awesome-debounce-promise";
 import * as yup from "yup";
 import { axiosInstance } from "./../api";
 
-export const productoSchema = yup.object().shape({
+const isUniqueNombre = adb(async value => {
+  try {
+    const res = await axiosInstance({
+      url: `/productos?filter[where][nombre]=${encodeURIComponent(value)}`,
+      method: "GET"
+    });
+    if (res.data.length === 0) {
+      return true;
+    }
+    return false;
+  } catch (err) {
+    return true;
+  }
+}, 500);
+
+const shape = {
   nombre: yup
     .string()
     .required("El elemento es requerido")
     .test(
       "unique_nombre",
       "Ya hay un producto registrado con ese nombre",
-      adb(async value => {
-        try {
-          const res = await axiosInstance({
-            url: `/productos?filter[where][nombre]=${encodeURIComponent(
-              value
-            )}`,
-            method: "GET"
-          });
-          if (res.data.length === 0) {
-            return true;
-          }
-          return false;
-        } catch (err) {
-          return true;
-        }
-      }, 500)
+      isUniqueNombre
     ),
   precio: yup
     .number()
@@ -39,4 +39,25 @@ export const productoSchema = yup.object().shape({
     .number()
     .required("El elemento es requerido")
     .nullable()
-});
+};
+
+export const productoSchema = yup.object().shape(shape);
+
+const editShape = {
+  nombre: yup.string().required("El elemento es requerido"),
+  precio: yup
+    .number()
+    .min(0.5, "El precio debe de ser mayor a 0")
+    .required("El elemento es requerido"),
+  descripcion: yup.string().required("El elemento es requerido"),
+  imagenUrl: yup
+    .string()
+    .nullable()
+    .notRequired(),
+  categoriaId: yup
+    .number()
+    .required("El elemento es requerido")
+    .nullable()
+};
+
+export const editProductoSchema = yup.object().shape(editShape);
